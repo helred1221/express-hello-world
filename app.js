10e1fb0a81d793369e41273563b545d02f64476e
Helder45 <helder.pereira@estudante.ifms.edu.br>
2024-11-15 12:20:42 -0400

    chore: finished app

--- app.js
+++ app.js
@@ -2,16 +2,74 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
-const json = {
-    msg: 'Hello World!'
-}
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-    res.json(json.msg);
-});
+app.get("/", (req, res) => res.type('html').send(html));
 
-// app.listen(port, () => {
-//     console.log('Hello World!');
-// });
+app.get('/req', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
 
 app.listen(port, () => console.log(`Aplicação rodando na porta ${port}!`));
+
+const html = `<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World App</title>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.3/dist/confetti.browser.min.js"></script>
+    <script>
+        setTimeout(() => {
+            confetti({
+                particleCount: 100,
+                spread: 70,
+                origin: { y: 0.6 },
+                disableForReducedMotion: true
+            });
+        }, 500);
+    </script>
+    <style>
+        @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
+
+        @font-face {
+            font-family: "neo-sans";
+            src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+            font-style: normal;
+            font-weight: 700;
+        }
+
+        html {
+            font-family: neo-sans;
+            font-weight: 700;
+            font-size: calc(62rem / 16);
+        }
+
+        body {
+            background: white;
+        }
+
+        section {
+            border-radius: 1em;
+            padding: 1em;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            margin-right: -50%;
+            transform: translate(-50%, -50%);
+        }
+    </style>
+</head>
+
+
+<body>
+    <section>
+        Hello Express API
+    </section>
+</body>
+
+</html>
+`
