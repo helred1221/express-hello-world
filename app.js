4517d29a5f5c9740f32252bf3accc7c32d729989
Helder45 <helder.pereira@estudante.ifms.edu.br>
2024-11-15 11:32:42 -0400

    fix: rewritten code for response

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
+const json = {
+    msg: 'Hello World!'
+}
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.json(json.msg);
 });
+
+// app.listen(port, () => {
+//     console.log('Hello World!');
+// });
 
 app.listen(port, () => console.log(`Aplicação rodando na porta ${port}!`));
